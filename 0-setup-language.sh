6c9e5e3c0ea5b1401b9b95cc9972be03f14ead75
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-02-25 20:13:06 +0100

    fixed setting up language

--- 0-setup-language.sh
+++ 0-setup-language.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo locale-gen "en_US.UTF-8"
-sudo -c "echo >> /etc/environment"
-sudo -c "echo LC_ALL=en_US.UTF-8 >> /etc/environment"
-sudo -c "echo LANG=en_US.UTF-8>> /etc/environment"
+sudo -u root "echo >> /etc/environment"
+sudo -u root "echo LC_ALL=en_US.UTF-8 >> /etc/environment"
+sudo -u root "echo LANG=en_US.UTF-8>> /etc/environment"
