f51df8dbd1c2f32fb1417eaccdee05cc319fde25
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-02-25 20:29:05 +0100

    Better docker install script

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Installing docker...
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
@@ -6,22 +7,24 @@ sudo apt-get update
 sudo apt-get install -y docker-engine
 
 #status of docker
-sudo systemctl status docker
+echo You can check status of docker service with:
+echo sudo systemctl status docker
 
 #add current user to docker group to use without sudo
+echo Adding current user to docker group. You have to relog to make this work
 sudo usermod -aG docker $USER
 
+echo Installing docker-compose
 sudo sh -c 'curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose'
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo You have to edit /lib/systemd/system/docker.service to change storage drive to device mapper
-exit 0
-# or /lib/systemd/system/docker.service
-# [Service]
-# ExecStart=/usr/bin/docker daemon --storage-driver=devicemapper -H fd://
+echo Changing storage driver to devicemapper
+sed -i -e '/^ExecStart=/ s/$/ --storage-driver=devicemapper/' /lib/systemd/system/docker.service
 
+echo Reloading services
 sudo service docker restart
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-docker info | grep Storage\ Driver
+echo Current storage driver is: (be worried if its not storage driver)
+sudo docker info | grep Storage\ Driver
