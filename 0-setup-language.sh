15d73ecdeb49783acbb36293f7547213a2040da5
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-03-18 22:30:38 +0100

    Added swap creation

--- 0-setup-language.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo locale-gen "en_US.UTF-8"
-sudo sh -c "echo >> /etc/environment"
-sudo sh -c "echo LC_ALL=en_US.UTF-8 >> /etc/environment"
-sudo sh -c "echo LANG=en_US.UTF-8>> /etc/environment"
