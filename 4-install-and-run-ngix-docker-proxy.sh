744daff9be1487261b762701c3afb92963d298a2
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-03-18 22:59:06 +0100

    Fixed a nginx-proxy container name

--- 4-install-and-run-ngix-docker-proxy.sh
+++ 4-install-and-run-ngix-docker-proxy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --restart=unless-stopped name=nginx_proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run --restart=unless-stopped --name=nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
