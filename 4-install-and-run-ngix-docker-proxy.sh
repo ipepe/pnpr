782cdcbe797c89b524b8d7945ee10d82092706bc
Patryk Ptasinski <github@ipepe.pl>
2018-05-06 12:36:31 +0200

    Cleanup 4th script

--- 4-install-and-run-ngix-docker-proxy.sh
+++ 4-install-and-run-ngix-docker-proxy.sh
@@ -4,7 +4,7 @@ mkdir -p /opt
 
 cp -r src/nginx-proxy-conf/ /opt/
 
-docker run --restart=unless-stopped -d -p 80:80 -p 443:443 \
+docker run --restart=always -d -p 80:80 -p 443:443 \
     --name nginx \
     -v /opt/nginx-proxy-conf/certs:/opt \
     -v /etc/nginx/conf.d  \
@@ -14,7 +14,7 @@ docker run --restart=unless-stopped -d -p 80:80 -p 443:443 \
     --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy \
     nginx
 
-docker run --restart=unless-stopped -d \
+docker run --restart=always -d \
     --name nginx-gen \
     --volumes-from nginx \
     -v /opt/nginx-proxy-conf/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro \
@@ -23,13 +23,9 @@ docker run --restart=unless-stopped -d \
     jwilder/docker-gen \
     -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
-docker run --restart=unless-stopped -d \
+docker run --restart=always -d \
      --name nginx-letsencrypt \
      --volumes-from nginx \
      -v /path/to/certs:/etc/nginx/certs:rw \
      -v /var/run/docker.sock:/var/run/docker.sock:ro \
      jrcs/letsencrypt-nginx-proxy-companion
-
-
-sudo docker run -d --name whoami -h whoami -e VIRTUAL_HOST=whoami.ipepe.pl -e LETSENCRYPT_HOST=whoami.ipepe.pl -e LETSENCRYPT_EMAIL=letsencrypt@ipepe.pl --restart=unless-stopped -i -t -P jwilder/whoami
-sudo docker run -d --name whoami2 -h whoami2 -e VIRTUAL_HOST=whoami2.ipepe.pl -e LETSENCRYPT_HOST=whoami2.ipepe.pl -e LETSENCRYPT_EMAIL=letsencrypt@ipepe.pl --restart=unless-stopped -i -t -P jwilder/whoami
