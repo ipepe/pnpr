a8468226de2e40b1892b2777fc8ff1516e399bad
Patryk Ptasinski <patryk@ipepe.pl>
2019-04-12 15:54:18 +0200

    Updated install docker script

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
-sudo apt-get install -y docker-engine
+sudo apt-get install -y docker-engine zip unzip
 
 #status of docker
 echo You can check status of docker service with:
@@ -15,10 +15,10 @@ echo "Adding current user to docker group. You have to relog to make this work"
 sudo usermod -aG docker $USER
 
 echo "Installing docker-compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo sh -c 'curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose'
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "Changing storage driver to devicemapper"
+echo "Changing storage driver to overlay2"
 systemctl stop docker
 sudo rm -rf /var/lib/docker
 sudo sed -i -e '/^ExecStart=/ s/$/ --storage-driver=overlay2/' /lib/systemd/system/docker.service
