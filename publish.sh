50ebdd6a7b53f3d70926be2684a97c1bf02700ee
Patryk Ptasinski <patryk@ipepe.pl>
2022-09-28 17:27:57 +0200

    Many ruby versions in publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-docker build . -t ipepe/pnpr:v2-production \
-          --build-arg RAILS_ENV=production \
-          --build-arg NODE_ENV=production \
-          --build-arg FRIENDLY_ERROR_PAGES=off \
-          --build-arg WITH_SUDO=false
-docker push ipepe/pnpr:v2-production
-docker build . -t ipepe/pnpr:v2-staging
-docker push ipepe/pnpr:v2-staging
+set -e
+
+RUBY_VERSIONS=(2.3.8 2.4.10 2.5.9 2.6.10 2.7.5)
+
+for RUBY_VERSION in "${RUBY_VERSIONS[@]}"; do
+  echo "Building ruby ipepe/pnpr:v2-staging-ruby-$RUBY_VERSION"
+  docker build . -t "ipepe/pnpr:v2-staging-ruby-$RUBY_VERSION" --build-arg RUBY_VERSION=$RUBY_VERSION
+  docker push "ipepe/pnpr:v2-staging-ruby-$RUBY_VERSION"
+
+  docker build . -t "ipepe/pnpr:v2-production-ruby-$RUBY_VERSION" \
+            --build-arg RUBY_VERSION=$RUBY_VERSION \
+            --build-arg RAILS_ENV=production \
+            --build-arg NODE_ENV=production \
+            --build-arg FRIENDLY_ERROR_PAGES=off \
+            --build-arg WITH_SUDO=false
+  docker push "ipepe/pnpr:v2-production-ruby-$RUBY_VERSION"
+done
+
+
+
