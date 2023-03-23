e97e47692c22c6452eb6182c84257008edfe0171
Patryk Ptasinski <patryk@ipepe.pl>
2023-03-24 00:39:33 +0100

    Change foreman script

--- rootfs/docker-entrypoint.rb
+++ rootfs/docker-entrypoint.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 # this script has 3 purposes:
-# 1. prepare container (file permissions, etc) and start all relevant services in proper order
-# 2. when receiving interrupt, forward this interrupt to all child processes
+# 1. when receiving interrupt, forward this interrupt to all child processes
+# 2. prepare container (file permissions, etc) and start all relevant services in proper order
 # 3. reap all zombie/defunct processes
 
 def log(message)
@@ -13,19 +13,6 @@ def logged_system_call(command)
   log("Executing: #{command}")
   system(command)
 end
-
-# ==== PREPARE CONTAINER AND START SERVICES ====
-SERVICE_NAMES = [:ssh, :"redis-server", :cron, :nginx, :"passenger-exporter"].freeze
-
-logged_system_call("bash /erb.templates/render.sh")
-logged_system_call("bash /bootstrap.sh")
-logged_system_call('chown -R webapp:webapp "/home/webapp" &')
-
-SERVICE_NAMES.each do |service_name|
-  logged_system_call("service #{service_name} start")
-end
-
-log "All services started. Waiting for interrupt..."
 
 # ==== RECEIVE AND FORWARD INTERRUPT SIGNALS TO CHILD PROCESSES ====
 [:INT, :QUIT, :TERM].each do |signal|
@@ -39,6 +26,19 @@ log "All services started. Waiting for interrupt..."
     exit(Signal.list[signal.to_s])
   end
 end
+
+# ==== PREPARE CONTAINER AND START SERVICES ====
+SERVICE_NAMES = [:ssh, :"redis-server", :cron, :nginx, :"passenger-exporter", :foreman].freeze
+
+logged_system_call("bash /erb.templates/render.sh")
+logged_system_call("bash /bootstrap.sh")
+logged_system_call('chown -R webapp:webapp "/home/webapp" &')
+
+SERVICE_NAMES.each do |service_name|
+  logged_system_call("service #{service_name} start")
+end
+
+log "All services started. Waiting for interrupt..."
 
 # ==== REAP ALL ZOMBIE AND DEFUNCT SERVICES ====
 Process.waitall
--- rootfs/erb.templates/templates/foreman.rb
+++ rootfs/erb.templates/templates/foreman.rb
@@ -7,83 +7,70 @@ FILE_PATH = "/etc/init.d/foreman".freeze
 RAILS_ENV = ENV.fetch("RAILS_ENV", "production")
 
 TEMPLATE = <<~ERB.freeze
-  #!/bin/bash
+  #!/bin/sh
   ### BEGIN INIT INFO
   # Provides:          foreman
-  # Required-Start:    $network $local_fs $remote_fs
-  # Required-Stop:     $network $local_fs $remote_fs
-  # Should-Start:      $syslog
-  # Should-Stop:       $syslog
+  # Required-Start:    $local_fs $network $named $time $syslog
+  # Required-Stop:     $local_fs $network $named $time $syslog
   # Default-Start:     2 3 4 5
   # Default-Stop:      0 1 6
-  # Short-Description: Manage Foreman process manager
-  # Description:       Manages Foreman as a process manager daemon.
+  # Description:       Start Foreman to manage the webapp process
   ### END INIT INFO
   
-  # Customize this based on your bundler location, app directory, etc.
-  APP_ROOT="/home/webapp/webapp/current"
-  PIDFILE="/home/webapp/webapp/shared/foreman.pid"
-  LOGFILE="$APP_ROOT/log/foreman.log"
-  RAILS_ENV="<%= RAILS_ENV %>"
-  FOREMAN_OPTIONS="-e $RAILS_ENV start -f $APP_ROOT/Procfile"
+  APP_NAME="foreman"
+  APP_DIR="/home/webapp/webapp/current"
+  APP_USER="webapp"
+  PID_DIR="/home/webapp/webapp/shared"
+  PID_FILE="$PID_DIR/$APP_NAME.pid"
+  LOG_DIR="$APP_DIR/log"
+  LOG_FILE="$LOG_DIR/$APP_NAME.log"
   BUNDLE_BIN="/home/webapp/.rbenv/shims/bundle"
-  FOREMAN_BIN="$BUNDLE_BIN exec foreman"
+  FOREMAN_CMD="$BUNDLE_BIN exec foreman"
+  RAILS_ENV="<%= RAILS_ENV %>"
   
-  set -e
+  . /lib/lsb/init-functions
   
   start() {
-    cd $APP_ROOT
-    touch $PIDFILE # create empty PID file
-    chown webapp:webapp $PIDFILE # change ownership of PID file
-    start-stop-daemon --start --chuid webapp:webapp --chdir $APP_ROOT --pidfile $PIDFILE --make-pidfile --background --exec $FOREMAN_BIN -- $FOREMAN_OPTIONS >>$LOGFILE 2>&1
-    echo "Starting Foreman..."
-    sleep 1
-    if [ -f $PIDFILE ]; then
-      echo "Foreman started with PID `cat $PIDFILE`."
-    else
-      echo "Failed to start Foreman."
-    fi
+      log_daemon_msg "Starting $APP_NAME"
+  
+      if [ ! -d "$PID_DIR" ]; then
+          mkdir -p "$PID_DIR"
+          chown "$APP_USER":"$APP_USER" "$PID_DIR"
+      fi
+  
+      if [ ! -d "$LOG_DIR" ]; then
+          mkdir -p "$LOG_DIR"
+          chown "$APP_USER":"$APP_USER" "$LOG_DIR"
+      fi
+  
+      start-stop-daemon --start --background --quiet --pidfile "$PID_FILE" --make-pidfile --chuid "$APP_USER" --exec /bin/sh -- -c "export RAILS_ENV='$RAILS_ENV'; cd '$APP_DIR' && exec $FOREMAN_CMD start -f '$APP_DIR/Procfile' -l '$LOG_DIR' -p '$PID_DIR'"
+  
+      log_end_msg $?
   }
   
   stop() {
-    cd $APP_ROOT
-    start-stop-daemon --stop --user webapp --pidfile $PIDFILE --remove-pidfile
-    echo "Stopping Foreman..."
-    sleep 1
-    if [ ! -f $PIDFILE ]; then
-      echo "Foreman stopped."
-    else
-      echo "Failed to stop Foreman."
-    fi
-  }
+      log_daemon_msg "Stopping $APP_NAME"
   
-  status() {
-    cd $APP_ROOT
-    if [ -f $PIDFILE ]; then
-      echo "Foreman is running with PID `cat $PIDFILE`."
-    else
-      echo "Foreman is not running."
-    fi
+      start-stop-daemon --stop --quiet --pidfile "$PID_FILE"
+  
+      log_end_msg $?
   }
   
   case "$1" in
-    start)
-      start
-      ;;
-    stop)
-      stop
-      ;;
-    restart)
-      stop
-      start
-      ;;
-    status)
-      status
-      ;;
-    *)
-      echo "Usage: $0 {start|stop|restart|status}"
-      exit 1
-      ;;
+      start)
+          start
+          ;;
+      stop)
+          stop
+          ;;
+      restart)
+          stop
+          start
+          ;;
+      *)
+          echo "Usage: $0 {start|stop|restart}"
+          exit 1
+          ;;
   esac
   
   exit 0
