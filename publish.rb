d2f8ac4e9def92ce4eaeffbdcfbb9c78722998f4
Patryk Ptasinski <patryk@ipepe.pl>
2023-09-18 23:39:57 +0200

    Closer relation between ruby and node versions

--- publish.rb
+++ publish.rb
@@ -1,36 +1,34 @@
 #!/usr/bin/env ruby
-RUBY_VERSIONS = [
-  "2.3.1", "2.3.8",
-  "2.4.10",
-  "2.5.9",
-  "2.6.10",
-  "2.7.2", "2.7.5", "2.7.6", "2.7.7", "2.7.8",
-  "3.0.4", "3.0.5", "3.0.6",
-  "3.1.2", "3.1.3", "3.1.4",
-  "3.2.2"
+
+RUBY_NODE_VERSIONS = [
+  "3.2.2" => ["16", "18", "20"],
+  "3.1.4" => ["16", "18", "20"],
+  "3.0.6" => ["16", "18", "20"],
+  "2.7.8" => ["10", "12", "14", "16", "18"],
+  "2.6.10" => ["10", "12", "14", "16", "18"],
+  "2.5.9" => ["10", "12", "14", "16", "18"],
+  "2.4.10" => ["10", "12", "14", "16", "18"],
+  "2.3.8" => ["10", "12", "14", "16", "18"],
+  "2.3.1" => ["10", "12", "14", "16", "18"],
 ].freeze
 
-NODE_VERSIONS = ["10", "12", "14", "16", "18"].freeze
-
-NODE_VERSIONS.each do |node_version|
-  RUBY_VERSIONS.each do |ruby_version|
-    image_tag = "v3.4-u20.04-r#{ruby_version}-n#{node_version}"
-    image_name = "ipepe/pnpr:#{image_tag}"
-    env_level = "production"
-    friendly_error_pages =
-      if env_level == "production"
-        "off"
-      else
-        "on"
-      end
-    puts "Building #{image_name}"
-    `docker build . --tag "#{image_name}" \
+RUBY_NODE_VERSIONS.each do |ruby_version|
+  image_tag = "v3.3-u20.04-r#{ruby_version}-n#{node_version}"
+  image_name = "ipepe/pnpr:#{image_tag}"
+  env_level = "production"
+  friendly_error_pages =
+    if env_level == "production"
+      "off"
+    else
+      "on"
+    end
+  puts "Building #{image_name}"
+  `docker build . --tag "#{image_name}" \
             --build-arg RUBY_VERSION=#{ruby_version} \
             --build-arg NODE_MAJOR_VERSION=#{node_version} \
             --build-arg RAILS_ENV=#{env_level} \
             --build-arg NODE_ENV=#{env_level} \
             --build-arg FRIENDLY_ERROR_PAGES=#{friendly_error_pages}`
-    puts "Pushing #{image_name}"
-    `docker push "#{image_name}"`
-  end
+  puts "Pushing #{image_name}"
+  `docker push "#{image_name}"`
 end
