a128e3707716f6a27b80eb707092a9a7b36dceab
Patryk Ptasinski <patryk@ipepe.pl>
2023-09-18 23:41:52 +0200

    Fix publish.rb

--- publish.rb
+++ publish.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-RUBY_NODE_VERSIONS = [
+{
   "3.2.2" => ["16", "18", "20"],
   "3.1.4" => ["16", "18", "20"],
   "3.0.6" => ["16", "18", "20"],
@@ -10,25 +10,25 @@ RUBY_NODE_VERSIONS = [
   "2.4.10" => ["10", "12", "14", "16", "18"],
   "2.3.8" => ["10", "12", "14", "16", "18"],
   "2.3.1" => ["10", "12", "14", "16", "18"],
-].freeze
-
-RUBY_NODE_VERSIONS.each do |ruby_version, node_version|
-  image_tag = "v3.3-u20.04-r#{ruby_version}-n#{node_version}"
-  image_name = "ipepe/pnpr:#{image_tag}"
-  env_level = "production"
-  friendly_error_pages =
-    if env_level == "production"
-      "off"
-    else
-      "on"
-    end
-  puts "Building #{image_name}"
-  `docker build . --tag "#{image_name}" \
+}.each do |ruby_version, node_versions|
+  node_versions.each do |node_version|
+    image_tag = "v3.3-u20.04-r#{ruby_version}-n#{node_version}"
+    image_name = "ipepe/pnpr:#{image_tag}"
+    env_level = "production"
+    friendly_error_pages =
+      if env_level == "production"
+        "off"
+      else
+        "on"
+      end
+    puts "Building #{image_name}"
+    `docker build . --tag "#{image_name}" \
             --build-arg RUBY_VERSION=#{ruby_version} \
             --build-arg NODE_MAJOR_VERSION=#{node_version} \
             --build-arg RAILS_ENV=#{env_level} \
             --build-arg NODE_ENV=#{env_level} \
             --build-arg FRIENDLY_ERROR_PAGES=#{friendly_error_pages}`
-  puts "Pushing #{image_name}"
-  `docker push "#{image_name}"`
+    puts "Pushing #{image_name}"
+    `docker push "#{image_name}"`
+  end
 end
